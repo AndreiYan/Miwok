7c92292541f3db1a9c27b03feadfe43edabd7aa0
AndreiYan <38234980+AndreiYan@users.noreply.github.com>
2018-08-02 11:52:50 -0400

    Update ColorsActivity.java

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -15,7 +15,7 @@ public class ColorsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.list_item);
+        setContentView(R.layout.word_list);
 
         //Array of the numbers
         ArrayList<Word> words = new ArrayList<Word>();
